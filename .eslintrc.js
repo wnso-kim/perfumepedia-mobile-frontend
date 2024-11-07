19bd05c4383abc1fde6f40f2acf71f1311cb87fa
byunjiin <jibyun5808@naver.com>
2024-11-07 15:42:18 +0900

    🔧 Config : eslint & prettier 설정

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: "airbnb",
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "arrow-body-style": "off",
+    "no-console": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [1, { extensions: [".js", "jsx"] }],
+    "react/prop-types": "off",
+    "linebreak-style": 0,
+    "react/jsx-props-no-spreading": "off",
+    "react/jsx-one-expression-per-line": "off",
+    camelcase: "off",
+    "operator-linebreak": "off",
+    "import/no-extraneous-dependencies": "off",
+    "no-multiple-empty-lines": "off",
+    "implicit-arrow-linebreak": "off",
+    "no-confusing-arrow": "off",
+    "function-paren-newline": "off",
+    "object-curly-newline": "off",
+    "react/no-children-prop": "off",
+    indent: "off",
+  },
+};
