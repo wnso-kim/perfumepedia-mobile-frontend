0643c9a9fb2003bca2899ae93627dcac4c5846a0
byunjiin <jibyun5808@naver.com>
2024-12-19 23:13:47 +0900

    🔧 Config : 공동 색상 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
       colors: {
         gray: {
           d9: "#D9D9D9",
+          c9: "#C9C9C9",
+          33: "#333236",
+          a6: "#A6A6A6",
         },
       },
     },
